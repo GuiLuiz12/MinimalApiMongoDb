8dd8bf74ab2dc2fc2c2a11ab3fae5efd694a5dbf
Guilherme Luiz Da Silva <36992072854@info.local>
2024-07-26 17:20:40 -0300

    api

---
+++ minimalAPIMongo/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using minimalAPIMongo.Domains;
+using minimalAPIMongo.Services;
+using MongoDB.Driver;
+
+namespace minimalAPIMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMongoCollection<Product> _product;
+
+        public ProductController(MongoDbService mongoDbService)
+        {
+            _product = mongoDbService.GetDatabase.GetCollection<Product>("product");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            try
+            {
+                var products = await _product.Find(FilterDefinition<Product>.Empty).ToListAsync();
+                return Ok(products);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Product product)
+        {
+            try
+            {
+                await _product.InsertOneAsync(product);
+                return StatusCode(201, product);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            try
+            {
+                var filter = Builders<Product>.Filter.Eq("Id", id);
+                var result = await _product.DeleteOneAsync(filter);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
